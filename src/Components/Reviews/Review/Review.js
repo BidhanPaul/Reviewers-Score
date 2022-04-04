ab1cdfe8eb573dbf7ed41e43122e3df3bd251821
Bidhan Paul <bidhanpaul7@gmail.com>
2022-04-04 22:57:35 +0600

    Review Star added

--- src/Components/Reviews/Review/Review.js
+++ src/Components/Reviews/Review/Review.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './Review.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faStar } from '@fortawesome/free-solid-svg-icons'
 
 const Review = (props) => {
     const { img, name, product, brand, review, comment } = props.review;
@@ -10,6 +12,13 @@ const Review = (props) => {
             <p><strong>Product:</strong> {product}</p>
             <p><strong>Brand:</strong> {brand}</p>
             <p><strong>Review:</strong> {review}</p>
+            <p><small className='star-color'>
+                <FontAwesomeIcon icon={faStar} />
+                <FontAwesomeIcon icon={faStar} />
+                <FontAwesomeIcon icon={faStar} />
+                <FontAwesomeIcon icon={faStar} />
+                <FontAwesomeIcon icon={faStar} />
+            </small></p>
             <p><strong>Comment:</strong> {comment}</p>
 
         </div>
