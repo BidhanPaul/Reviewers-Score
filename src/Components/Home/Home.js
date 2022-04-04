03337877cc784840cff8fdbc860f73d1a78557e3
Bidhan Paul <bidhanpaul7@gmail.com>
2022-04-04 22:34:56 +0600

    Home page review fixed

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -26,7 +26,7 @@ const Home = () => {
                 </div>
                 <div className='review'>
                     {
-                        reviews.map(review => <Review
+                        reviews.slice(0, 3).map(review => <Review
                             key={review.id}
                             review={review}
                         ></Review>)
