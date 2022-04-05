aac0b7b4e8376e232c0e954715981855459de152
Bidhan Paul <bidhanpaul7@gmail.com>
2022-04-05 13:22:35 +0600

    Blog Added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './Components/Home/Home';
 import NotFound from './Components/NotFound/NotFound';
 import Reviews from './Components/Reviews/Reviews';
 import Dashboard from './Components/Dashboard/Dashboard';
+import Blog from './Components/Blog/Blog';
 function App() {
   return (
     <div>
@@ -14,6 +15,7 @@ function App() {
         <Route path='/home' element={<Home></Home>}></Route>
         <Route path='/reviews' element={<Reviews></Reviews>}></Route>
         <Route path='/dashboard' element={<Dashboard></Dashboard>}></Route>
+        <Route path='/blog' element={<Blog></Blog>}></Route>
         <Route path='/*' element={<NotFound></NotFound>}></Route>
       </Routes>
     </div>
---
+++ src/Components/Blog/Blog.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import img3 from './img/React-Context.png';
+import img4 from './img/semantic_Tag.png';
+import './Blog.css'
+
+const Blog = () => {
+    return (
+        <div className='blog-container'>
+            <div className='blog'>
+                <div>
+                    <h1>What is Context API</h1>
+                    <p>In React, the context API is a method for React application functionally generate Global variables that can be passed around. This is an alternative to “Prop drilling” or shifting props to grandparent to child parent etc. This is an easier and lighter approach to state management.</p>
+                </div>
+                <div className='img-1'>
+                    <img src={img3} alt="" />
+                </div>
+            </div>
+            <div className='blog'>
+                <div>
+                    <h1>What is Semantic Tag</h1>
+                    <p>Semantic tag are some special HTML tags that makes a HTML page meaningful. Each tags describes its meaning to the browser and also to the developer. These tags are so important because that makes the HTML more comprehensible by better describing the different section and layout of web pages.</p>
+                </div>
+                <div>
+                    <img src={img4} alt="" />
+                </div>
+            </div>
+            <div className='blog'>
+                <h1>Inline, Inline-Block, Block Elements</h1>
+                <p><strong>Inline:</strong> The element doesn’t start on a new line and only occupy just the width it requires. You can’t set the width or height.
+                    <br />
+                    <strong>Inline-block:</strong> it is formatted just like the inline element, where it doesn’t start on a new line. But you can apply width and height values.
+                    <br />
+                    <strong>Block:</strong> The element will start on a new line and occupy the full width available. And you can set width and height values.
+                </p>
+            </div>
+        </div>
+    );
+};
+
+export default Blog;
